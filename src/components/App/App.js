e1c29a921ddab5edac94c53ff8003948412a43ef
saritaSchen <sarita.schenkermayr@gmail.com>
2022-05-28 11:05:14 +0200

    Change wording on Landing Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import React from "react"
 function App () {
     return (
         <div className="App">
-            <h1>My Ingredient Tracker</h1>
+            <h1>Ingredient Tracker Landing Page</h1>
         </div>
     );
 }
