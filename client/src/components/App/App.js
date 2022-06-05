29e15624cc8493bced1f26ef56b10f3a0d6848b6
saritaSchen <sarita.schenkermayr@gmail.com>
2022-06-05 14:27:11 +0200

    add Groceries Component with Search bar and let it show in App.js

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import DemoData from '../DemoComponent';
+import Groceries from "../Groceries/Groceries";
 
 function App () {
     return (
         <div className="App">
             <h1>Ingredient Tracker Landing Page</h1>
+            <Groceries />
             < DemoData />
         </div>
     );
---
+++ client/src/components/Groceries/Groceries.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const addIngredient = (ingredient) => {
+    
+}
+
+const Groceries = () => {
+    return (
+        <div className="searchbar">
+            <label>Search</label>
+            <input type="text"/>
+            <button>Add</button>
+        </div>
+    );
+}
+
+export default Groceries;
