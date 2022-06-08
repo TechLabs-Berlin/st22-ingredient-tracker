5d477cc1a5f1341d6935350aebca764a565dcd5c
saritaSchen <sarita.schenkermayr@gmail.com>
2022-06-08 09:39:27 +0200

    test Material CSS Framework

--- client/src/components/GroceriesSearch/GroceriesSearch.js
+++ client/src/components/GroceriesSearch/GroceriesSearch.js
@@ -1,11 +1,20 @@
 import React from "react";
+//import {MDCFoo, MDCFooFoundation} from '@material/foo';
 
 const GroceriesSearch = () => {
     return (
         <div className="searchbar">
-            <label>Search</label>
-            <input type="text"/>
-            <button action="submit">Add</button>
+            <div>
+                <label class="mdc-text-field mdc-text-field--filled mdc-floating-label">
+                    <span class="mdc-floating-label">Search</span>
+                <input class="mdc-text-field__input" type="text"/>
+                </label>
+            </div>
+            <button action="submit" class="mdc-button mdc-button--raised">Add</button>
+    <label class="mdc-text-field mdc-text-field--filled">
+  <span class="mdc-floating-label" id="my-label-id">Hint text</span>
+  <input class="mdc-text-field__input" type="text" aria-labelledby="my-label-id"/>
+</label>
         </div>
     );
 }
