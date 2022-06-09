c494e32b23031eb2f5c97c5fb9493e1bde1c783f
catarinalouza <catarinalouza@gmail.com>
2022-06-09 16:46:08 +0200

    added styling to JSX

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -4,31 +4,48 @@ import React, { useState } from 'react';
 function LoginForm() {
 
     return (
-        <div className='form'>
-            <h1>Login</h1>
+        <div className='form card card m-4 p-4'>
+            {/* Title */}
+            <h1 className='title m-2 mb-5 has-text-primary'>Login</h1>
 
+            {/* Input Fields */}
             <div>
-                <label>Email</label>
-                <input className='input' type='email'></input>
+                <div className='email m-2 my-4'>
+                    <label className='has-text-primary has-text-weight-semibold is-size-5' htmlFor='email'>Email</label>
+                    <input className='input is-primary is-medium' type='email' id='email' placeholder='Email' required></input>
+                </div>
 
-                <label>Password</label>
-                <input className='input' type='password'></input>
-                <p>Forgot password?</p>
+                <div className='password m-2 my-4'>
+                    <label className='has-text-primary has-text-weight-semibold is-size-5' htmlFor='password'>Password</label>
+                    <input className='input is-primary is-medium' type='password' id='password' placeholder='Password' required></input>
+                    <p className='is-clickable is-size-7 has-text-grey is-pulled-right m-1 mb-4'>Forgot password?</p>
+                </div>
             </div>
 
+            {/* Login Button */}
             <div>
-                <button>Login</button>
+                <button className='button is-primary is-medium m-2 my-4'>Login</button>
             </div>
 
-            <div>
-                <p>Or login using</p>
-                <p>Google</p>
-                <p>Facebook</p>
+
+            {/* Social Media Buttons */}
+            <div className='socials m-4 my-4'>
+                <p className='subtitle has-text-weight-light'>Or Login Using:</p>
+                <p className='buttons'>
+                    <button className='button is-info'>
+                        Facebook
+                    </button>
+
+                    <button className='button is-danger'>
+                        Google
+                    </button>
+                </p>
             </div>
 
-            <div>
+            {/* Registration Link */}
+            <div className='is-flex is-align-content-flex-start m-4 mb-4 mt-6'>
                 <p>Not a member yet?</p>
-                <button>Register here.</button>
+                <p className='is-clickable has-text-primary ml-1'>Register here.</p>
             </div>
         </div>
     )
