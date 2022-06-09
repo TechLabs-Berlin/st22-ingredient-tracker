525dda8dd1b46b4906c43dbb1fbcd3d27115ab13
catarinalouza <catarinalouza@gmail.com>
2022-06-09 13:22:24 +0200

    added styling to terms & conditions

--- client/src/components/RegistrationForm/RegistrationForm.js
+++ client/src/components/RegistrationForm/RegistrationForm.js
@@ -36,34 +36,34 @@ function RegistrationForm() {
         <div className="card is-flex-direction-column m-4 p-4 is-justify-content-center">
 
             <h1 className="title m-2 has-text-primary">Registration Form</h1>
-            <h2 className="subtitle m-2 mb-6">Register to save recipes.</h2>
+            <h2 className="subtitle m-2 mb-6">Register to save ingredients and recipes to your Ingreduce dashboard.</h2>
 
             <div>
                 <div className="username m-2">
                     <label className="form__label has-text-primary has-text-weight-semibold is-size-5" htmlFor="username">Username</label>
-                    <input className="input is-primary is-medium" type="text" value={username} onChange={(e) => handleInputChange(e)} id="username" placeholder="Username" />
+                    <input className="input is-primary is-medium" type="text" value={username} onChange={(e) => handleInputChange(e)} id="username" placeholder="Username" required />
                 </div>
 
                 <div className="email m-2 my-4">
                     <label className="form__label has-text-primary has-text-weight-semibold is-size-5" htmlFor="email">Email </label>
-                    <input className="input is-primary is-medium" type="email" id="email" value={email} onChange={(e) => handleInputChange(e)} placeholder="Email" />
+                    <input className="input is-primary is-medium" type="email" id="email" value={email} onChange={(e) => handleInputChange(e)} placeholder="Email" required />
                 </div>
 
                 <div className="password m-2 my-4">
                     <label className="form__label has-text-primary has-text-weight-semibold is-size-5" htmlFor="password">Password </label>
-                    <input className="input is-primary is-medium" type="password" id="password" value={password} onChange={(e) => handleInputChange(e)} placeholder="Password" />
+                    <input className="input is-primary is-medium" type="password" id="password" value={password} onChange={(e) => handleInputChange(e)} placeholder="Password" required />
                 </div>
 
                 <div className="confirm-password m-2 my-4">
                     <label className="form__label has-text-primary has-text-weight-semibold is-size-5" htmlFor="confirmPassword">Confirm Password </label>
-                    <input className="input is-primary is-medium" type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => handleInputChange(e)} placeholder="Confirm Password" />
+                    <input className="input is-primary is-medium" type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => handleInputChange(e)} placeholder="Confirm Password" required />
                 </div>
             </div>
 
             <div>
-                <h3 className="is-size-6 m-2 my-5 has-text-weight-medium has-text-grey">Terms & Conditions</h3>
-                <input type="checkbox" id="tac" onChange={(e) => handleInputChange(e)}></input>
-                <label className="checkbox" for="tac">Terms & Conditions</label>
+                <h3 className="is-size-6 m-2 mt-5 has-text-weight-medium has-text-grey">Terms & Conditions</h3>
+                <input className="m-2" type="checkbox" id="tac" onChange={(e) => handleInputChange(e)}></input>
+                <label className="checkbox m-2 mb-4 has-text-grey" for="tac">I agree with the Terms & Conditions.</label>
 
             </div>
 
