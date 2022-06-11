ccdf679f49a9b227f1e5a097a2e0079b86ce96d4
saritaSchen <sarita.schenkermayr@gmail.com>
2022-06-11 19:25:39 +0200

    some experimentation with the GroceriesList

--- client/src/components/GroceriesList/GroceriesList.js
+++ client/src/components/GroceriesList/GroceriesList.js
@@ -1,9 +1,29 @@
 import React from "react";
 import GroceriesSearch from "../GroceriesSearch/GroceriesSearch";
 
+let groceries = ["apples", "butter", "wheat flour"];
+
 const addIngredient = (ingredient) => {
-    
+    groceries.push(ingredient);
 }
+
+/*
+const GroceriesList = ({groceries}) => {
+    const list = groceries.map((item) => {
+        return (
+            <div class="box">
+                <ul>
+                    <li class="box"><span>Test{item}</span>
+                        <a class="button is-danger is-outlined">
+                            <span class="icon is-small"><i class="fas fa-trash"></i></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    });
+}
+*/
 
 const GroceriesList = () => {
     return (
@@ -14,10 +34,10 @@ const GroceriesList = () => {
                         <span class="icon is-small"><i class="fas fa-trash"></i></span>
                     </a>
                 </li>
-                <li class="box">wheat flour</li>
             </ul>
         </div>
     );
 }
+
 
 export default GroceriesList;
