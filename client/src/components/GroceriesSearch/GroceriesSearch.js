7302192bf0aa4cbe7ecd0cbc2822ed1616e5482f
saritaSchen <sarita.schenkermayr@gmail.com>
2022-06-11 11:56:18 +0200

    start implementation of useState and useEffect for search component

--- client/src/components/GroceriesSearch/GroceriesSearch.js
+++ client/src/components/GroceriesSearch/GroceriesSearch.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 const GroceriesSearch = () => {
+    const [term, setTerm] = useState("");
+
+    //run when component is first rendered and when the search term changes
+    useEffect(() => {
+        const search = async() => {
+            await axios.get("thisisnotarealURLyet");
+        };
+
+        search();
+        
+    }, [term]);
+
     return (
         <div class="box"> 
             <label class="label">Search</label>
                 <div class="field">
                     <p class="control has-icons-left has-icons-right">
-                    <span class="icon has-text-primary"><i class="fas fa-search"></i></span>
-                    <input class="input is-rounded is-primary" type="search" placeholder="search ingredient"/>
+                        <span class="icon has-text-primary"><i class="fas fa-search"></i></span>
+                        <input 
+                            value={term}
+                            onChange={e => setTerm(e.target.value)}
+                            class="input is-rounded is-primary" 
+                            type="search" 
+                            placeholder="search ingredient"
+                        />
                     </p>
                 </div>
             <button class="button is-primary" action="submit">Add</button>
