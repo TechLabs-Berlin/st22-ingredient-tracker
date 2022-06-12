c5280f372eef7dc28002807368bc1a4cc4d7b89a
saritaSchen <sarita.schenkermayr@gmail.com>
2022-06-12 19:37:11 +0200

    start footer

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import DemoData from '../DemoComponent';
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 function App () {
     return (
         <div className="App">
             <Header />
             < DemoData />
+            <Footer />
         </div>
     );
 };
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <nav class="footer is-fixed-bottom">
+            <div class="navbar-brand">
+                <a class="navbar-item">
+                    <img src="https://www.vhv.rs/dpng/d/82-821105_cooking-png-icon-food-transparent-png.png"/>
+                    <h1 class="subtitle">Ingreduce</h1>
+                </a>
+            </div>
+
+            <div class="navbar-menu">
+
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                        About
+                    </a>
+
+                    <a class="navbar-item">
+                        Community
+                    </a>
+
+                    <a class="navbar-item">
+                        Company
+                    </a>
+                </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        Copyright
+                    </div>
+                </div>
+
+            </div>
+      
+        </nav>
+    );
+}
+
+export default Footer;
