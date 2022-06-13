16784108af8132929f8d691b60fee29fc2aa0df6
saritaSchen <sarita.schenkermayr@gmail.com>
2022-06-13 11:56:46 +0200

    let links in footer be bold text

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,24 +4,24 @@ const Footer = () => {
     return (
         <nav class="footer">
                 
-            <div class="navbar-menu">
-                <div class="navbar-brand">
+            <div class="navbar-menu columns">
+                <div class="navbar-brand column is-5">
                     <div class="navbar-item">
                         <img src="https://www.vhv.rs/dpng/d/82-821105_cooking-png-icon-food-transparent-png.png"/>
                         <h1 class="subtitle">Ingreduce</h1>
                     </div>
                 </div>
 
-                <div class="navbar-start ">
-                    <a class="navbar-item">
+                <div class="navbar-start">
+                    <a class="navbar-item has-text-weight-bold">
                         About
                     </a>
 
-                    <a class="navbar-item">
+                    <a class="navbar-item has-text-weight-bold">
                         Community
                     </a>
 
-                    <a class="navbar-item">
+                    <a class="navbar-item has-text-weight-bold">
                         Company
                     </a>
                 </div>
