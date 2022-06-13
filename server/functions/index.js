76435b9bad445e2960c4a7398cc2e2d26e36a5ba
alj-b <aljoscha_b@icloud.com>
2022-06-13 23:49:05 +0200

    Modified functions/index.js with Big Ben sample function from Firebase - Aljoscha

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,9 +1,15 @@
-const functions = require("firebase-functions");
+// Test https function as per Firebase documentation
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'BONG '.repeat(hours)}
+    </body>
+  </html>`);
+});
