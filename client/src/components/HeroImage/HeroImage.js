195e812e064d5625442bc7654ba834d1e64123b9
catarinalouza <catarinalouza@gmail.com>
2022-06-14 19:19:59 +0200

    added image and CSS file

--- client/src/components/HeroImage/HeroImage.js
+++ client/src/components/HeroImage/HeroImage.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './HeroImage.css'
+// import './photo_veggies.jpg'
 
 function HeroImage() {
 
     return (
-        <div>
-            set up
-        </div>
+        <section className="hero is-success is-halfheight">
+            <div className="hero-body">
+                <div className="">
+                    {/* <img src={require('./photo_veggies.jpg')} /> */}
+                    <p className="title">
+                        Ingreduce helps you reduce waste by providing healthy recipes using the ingredients you already have.
+                    </p>
+                    <button className='button'>Try Today</button>
+                </div>
+            </div>
+        </section>
     )
 };
 
