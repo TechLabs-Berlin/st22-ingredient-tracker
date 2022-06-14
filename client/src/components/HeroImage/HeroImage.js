b0317b70c5a7e006e03f470ad6702e5fa3839ace
catarinalouza <catarinalouza@gmail.com>
2022-06-14 20:11:38 +0200

    trying out image responsiveness

--- client/src/components/HeroImage/HeroImage.js
+++ client/src/components/HeroImage/HeroImage.js
@@ -5,14 +5,17 @@ import './HeroImage.css'
 function HeroImage() {
 
     return (
-        <section className="hero is-success is-halfheight">
+        <section className="hero is-halfheight">
             <div className="hero-body">
                 <div className="">
                     {/* <img src={require('./photo_veggies.jpg')} /> */}
-                    <p className="title">
+                    <p className="title has-text-white">
+                        Manage your pantry better!
+                    </p>
+                    <p className="subtitle has-text-white">
                         Ingreduce helps you reduce waste by providing healthy recipes using the ingredients you already have.
                     </p>
-                    <button className='button'>Try Today</button>
+                    <button className='button is-primary'>Try Today</button>
                 </div>
             </div>
         </section>
