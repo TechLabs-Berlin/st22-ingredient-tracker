870793044d0793ac5ab73afd90d79f8d1fdf7694
catarinalouza <catarinalouza@gmail.com>
2022-06-25 17:25:33 +0200

    initial button react set up

--- client/src/components/HeroImage/HeroImage.js
+++ client/src/components/HeroImage/HeroImage.js
@@ -2,19 +2,26 @@ import React from 'react';
 import './HeroImage.css'
 
 function HeroImage() {
+    function handleSubmit(event) {
+        event.preventDefault();
+    };
+
     return (
         <div>
-            {/* Hero Image and Button */}
+            {/* Hero Image */}
             <section className="hero is-large">
                 <div className="hero-body">
                     <div>
+                        {/* Title & Subtitle */}
                         <p className="title is-size-1 is-size-2-mobile has-text-primary has-text-weight-bold my-5">
                             Manage your pantry better!
                         </p>
                         <p className="is-size-4 is-size-5-mobile has-text-white mb-6">
                             Ingreduce helps you reduce waste by providing healthy recipes using the ingredients you already have.
                         </p>
-                        <button className='button is-primary has-text-weight-semibold my-5 is-size-5' style={{ width: '150px' }}>Try Today</button>
+
+                        {/* Button */}
+                        <button className='button is-primary has-text-weight-semibold my-5 is-size-5' style={{ width: '150px' }} type='submit'>Try Today</button>
                     </div>
                 </div>
             </section>
