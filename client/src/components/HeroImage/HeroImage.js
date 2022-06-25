064443019f371bb206ffc037ab2833442c206a00
catarinalouza <catarinalouza@gmail.com>
2022-06-25 17:11:57 +0200

    change color of title

--- client/src/components/HeroImage/HeroImage.js
+++ client/src/components/HeroImage/HeroImage.js
@@ -8,7 +8,7 @@ function HeroImage() {
             <section className="hero is-large">
                 <div className="hero-body">
                     <div>
-                        <p className="title is-size-1 is-size-3-mobile has-text-white has-text-weight-bold mb-4">
+                        <p className="title is-size-1 is-size-3-mobile has-text-primary has-text-weight-bold my-5">
                             Manage your pantry better!
                         </p>
                         <p className="is-size-4 is-size-5-mobile has-text-white mb-6">
@@ -18,28 +18,6 @@ function HeroImage() {
                     </div>
                 </div>
             </section>
-
-            {/*Instructions */}
-            {/* <section className=''>
-                <div className='hero-body'>
-
-                    <div>
-                        <p className='is-inline-block is-size-3 has-text-primary has-text-weight-bold'>1.</p>
-                        <p className='is-inline-block is-size-5 has-text-weight-normal ml-2'>Add the ingredients you want to use.</p>
-                    </div>
-
-                    <div>
-                        <p className='is-inline-block is-size-3 has-text-primary has-text-weight-bold'>2.</p>
-                        <p className='is-inline-block is-size-5 has-text-weight-normal ml-2'>Select a recipe to cook.</p>
-                    </div>
-
-                    <div>
-                        <p className='is-inline-block is-size-3 has-text-primary has-text-weight-bold'>3.</p>
-                        <p className='is-inline-block is-size-5 has-text-weight-normal ml-2'>Reduce your food waste!</p>
-                    </div>
-
-                </div>
-            </section> */}
         </div>
     )
 };
