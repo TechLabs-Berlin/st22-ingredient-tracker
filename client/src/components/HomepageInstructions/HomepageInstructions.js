2a5c67d79766d456937ffb1da5940ae2ff6c207a
catarinalouza <catarinalouza@gmail.com>
2022-06-29 15:35:19 +0200

    centered component

--- client/src/components/HomepageInstructions/HomepageInstructions.js
+++ client/src/components/HomepageInstructions/HomepageInstructions.js
@@ -3,26 +3,28 @@ import './HomepageInstructions.css'
 
 function HomepageInstructions() {
     return (
-        <div className='m-3'>
-            {/* Number 1 */}
-            <div className='block'>
-                <span class='tag is-primary is-large is-size-4 has-text-weight-bold is-rounded mr-2'>1</span>
-                <p className='is-inline-block subtitle is-size-4 has-text-weight-medium'>Add the ingredients you want to use.</p>
-            </div>
+        <div className='columns is-mobile is-vcentered' id='instructions'>
+            {/* <div className='column'></div> */}
+            <div className='column'>
+                {/* Number 1 */}
+                <div className='block'>
+                    <span class='tag is-primary is-large is-size-4 has-text-weight-bold is-rounded mr-2'>1</span>
+                    <p className='is-inline-block subtitle is-size-4 has-text-weight-medium'>Add the ingredients you want to use.</p>
+                </div>
 
-            {/* Number 2 */}
-            <div className='block'>
-                <span class='tag is-primary is-large is-size-4 has-text-weight-bold is-rounded mr-2'>2</span>
-                {/* <h1 className='is-inline-block title is-size-3 has-text-primary mr-2'>2.</h1> */}
-                <p className='is-inline-block subtitle is-size-4 has-text-weight-medium'>Select a recipe from the suggestions.</p>
-            </div>
+                {/* Number 2 */}
+                <div className='block'>
+                    <span class='tag is-primary is-large is-size-4 has-text-weight-bold is-rounded mr-2'>2</span>
+                    <p className='is-inline-block subtitle is-size-4 has-text-weight-medium'>Select a recipe from the suggestions.</p>
+                </div>
 
-            {/* Number 3 */}
-            <div className='block'>
-                <span class='tag is-primary is-large is-size-4 has-text-weight-bold is-rounded mr-2'>3</span>
-                <p className='is-inline-block subtitle is-size-4 has-text-weight-medium'>Save the recipe to your favourites!</p>
+                {/* Number 3 */}
+                <div className='block'>
+                    <span class='tag is-primary is-large is-size-4 has-text-weight-bold is-rounded mr-2'>3</span>
+                    <p className='is-inline-block subtitle is-size-4 has-text-weight-medium'>Save the recipe to your favourites.</p>
+                </div>
             </div>
-
+            {/* <div className='column'></div> */}
         </div >
     )
 }
