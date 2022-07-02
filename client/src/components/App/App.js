1f947d96dcae03793dcc911f6330bd9c93cffaaf
catarinalouza <catarinalouza@gmail.com>
2022-07-02 19:03:52 +0200

    set up of component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import GroceriesSearch from "../GroceriesSearch/GroceriesSearch";
 import GroceriesList from "../GroceriesList/GroceriesList";
+import RecipeOverview from "../RecipeOverview/RecipeOverview";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import DemoData from '../DemoComponent/DemoComponent';
@@ -9,18 +10,20 @@ import RegistrationForm from "../RegistrationForm/RegistrationForm";
 
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import RecipesOverview from "../RecipeOverview/RecipeOverview";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<DemoData />}/>
-                        <Route path="registration" element={<RegistrationForm />}/>
-                        <Route path="groceries" element={<><GroceriesSearch/><GroceriesList/></>}></Route>
-                    </Routes>
-                <Footer />
+                {/* <Header />
+                <Routes>
+                    <Route path="/" element={<DemoData />} />
+                    <Route path="registration" element={<RegistrationForm />} />
+                    <Route path="groceries" element={<><GroceriesSearch /><GroceriesList /></>}></Route>
+                </Routes> */}
+                <RecipesOverview />
+                {/* <Footer /> */}
             </div>
         </BrowserRouter>
     );
---
+++ client/src/components/RecipeOverview/RecipeOverview.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function RecipesOverview() {
+    return (
+        <div className='columns'>
+
+            <div className='column'>
+                <div className='card'>
+                    <div className='card-image'>
+                        <figure className='image'>
+                            <img src=''></img>
+                        </figure>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className='column'>
+                <div className='card'>
+                    <div className='card-image'>
+                        <figure>
+                            <img>
+                            </img>
+                        </figure>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className='column'>
+                <div className='card'>
+                    <div className='card-image'>
+                        <figure>
+                            <img>
+                            </img>
+                        </figure>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RecipesOverview;
