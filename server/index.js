39e50d95bdfeb17d5f52dc30bafda13f8015c94d
alj-b <aljoscha_b@icloud.com>
2022-07-12 16:51:38 +0200

    Started working on user model with local MongoDB database. Connected to MongoDB Atlas now - Aljoscha

--- server/index.js
+++ server/index.js
@@ -1,63 +1,88 @@
 const express = require('express');
 const cors = require('cors');
-const demoRouter = require('./routes/demoRoutes');
 const pantryRouter = require('./src/routes/pantryRoutes');
 const userRouter = require('./src/routes/userRoutes');
 const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const User = require('./src/models/user');
+const { default: mongoose } = require('mongoose');
+const bcrypt = require('bcrypt');
 
-// percent encoded password
+// percent encoded password for MongoDB Atlas
 
-const uri = "mongodb+srv://ingreduce_admin:<rice%26PASTA%3F%3D0Hmy>@ingreduce.nw3rh.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const mongoAtlasUri = "mongodb+srv://ingreduce_admin:rice%26PASTA%3F%3D0Hmy@ingreduce.nw3rh.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log(`Connected to MongoDB Atlas`);
-  client.close();
-});
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log(`Connected to MongoDB Atlas`);
+//   client.close();
+// });
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+
+// temporary local DB, to be replaced with the MongoDB Atlas address
+// MongooseServerSelectionError: connection <monitor> to 40.68.199.139:27017 closed | FOR DEVELOPMENT: had to give network access to all IP, change back later with dedicated server
+
+// mongoose.connect('mongodb://localhost:27017/demoIngreduce');
+
+try {
+    // Connect to the MongoDB cluster
+    mongoose.connect(
+      mongoAtlasUri,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Mongoose is connected"),
+    );
+  } catch (e) {
+    console.log("could not connect");
+  }
+  
+  const dbConnection = mongoose.connection;
+  dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
+  dbConnection.once("open", () => console.log("Connected to DB!"));
+
+// temporary, to be replaced by front end data
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+app.get('/register' , (req, res) => {
+    res.render('register');
+});
+
+app.get('/' , (req, res) => {
+    res.render('home');
+});
+
+app.post('/register', async (req, res) => {
+    const { password, username, email } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User ({
+        username,
+        email,
+        password: hash
+    });
+    await user.save();
+    res.redirect('/');
+})
 
 // signed cookies don't hide information, but add data to it so authenticity can be verified if need be
 app.use(cookieParser('secretsignthatshouldbestoredin.env')); // requires { signed : true } in route 
 
 const PORT = 5000;
 
-app.use('/demo', demoRouter);
 app.use('/pantry', pantryRouter);
 app.use('/user', userRouter);
+
+app.get('/secret', (req, res) => {
+    res.send('Secret');
+});
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDX-L4ifNg3FOpwyanhAdeAYk2f7L5zxx0",
-//   authDomain: "bl-st22--ingredient-racker.firebaseapp.com",
-//   projectId: "bl-st22--ingredient-racker",
-//   storageBucket: "bl-st22--ingredient-racker.appspot.com",
-//   messagingSenderId: "638075870453",
-//   appId: "1:638075870453:web:452ce173e823326f4939bc",
-//   measurementId: "G-YVB5X9GK1L"
-// };
-
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,3 +1,41 @@
 // username, email and password 
 // pantry and favourites 
-// send it to Firebase and associate with user
+// send it to Firebase/MongoDB atlas and associate with user
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: 
+    {
+        type: String,
+        maxlength: 20,
+        // unique: [true, 'Username has to be unique'],
+        // required: [true, 'Username cannot be empty']
+    },
+    email: 
+    {
+        type: String,
+        // unique: [true, 'Email has to be unique'],
+        // required: [true, 'Email cannot be empty']
+    },
+    password: 
+    {
+        type: String,
+        minlength: 6,
+        // required: [true, 'Password cannot be empty']
+    },
+    // pantry: [String],
+    // favourites: [String],
+    // userimg:
+    // {
+    //     data: Buffer,
+    //     contentType: String
+    // },
+    created: 
+    { 
+        type: Date,
+        required: [true, 'Creation date is required']
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
