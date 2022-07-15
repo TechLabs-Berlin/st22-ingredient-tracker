a1ff9624b43367490ec72bb1631b00eb6a7eeb39
saritaSchen <sarita.schenkermayr@gmail.com>
2022-07-15 20:58:53 +0200

    create demoRecipe object with recipe from recipe_database.json; change hardcoded content with keys from object; add some recipe elements that were missing

--- client/src/components/RecipeDetail/RecipeDetail.js
+++ client/src/components/RecipeDetail/RecipeDetail.js
@@ -2,6 +2,21 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const RecipeDetail = () => {
+    const demoRecipe = {
+        "recipe_link":"https://www.allrecipes.com/recipe/238691/simple-macaroni-and-cheese/",
+        "name":"Simple Macaroni and Cheese Recipe",
+        "description":"Quick, easy, and tasty macaroni and cheese dish. Fancy, designer mac and cheese often costs forty or fifty dollars to prepare when you have so many exotic and expensive cheeses, but they aren't always the best tasting. This simple recipe is cheap and tasty.",
+        "prep":"10 mins",
+        "cook":"15 mins",
+        "additional":null,
+        "total":"25 mins",
+        "servings":"4",
+        "yield":"4 servings",
+        "ingredients":"1 (8 ounce) box elbow macaroni; cup butter;  cup all-purpose flour; teaspoon salt; ground black pepper to taste; 2 cups milk; 2 cups shredded Cheddar cheese",
+        "instructions":"Bring a large pot of lightly salted water to a boil. Cook elbow macaroni in the boiling water, stirring occasionally until cooked through but firm to the bite, 8 minutes. At the same time, melt butter in a saucepan over medium heat. Add flour, salt, and pepper and stir until smooth, about 5 minutes. Pour in milk slowly, while stirring continuously. Continue to cook and stir until mixture is smooth and bubbling, about 5 minutes, making sure the milk doesn't burn. Add Cheddar cheese and stir until melted, 2 to 4 minutes. Drain macaroni and fold into cheese sauce until coated.","nutrition":"630 calories; protein 26.5g; carbohydrates 55g; fat 33.6g; cholesterol 99.6mg; sodium 777mg.",
+        "images_url":"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F03%2F19%2F238691-Simple-Macaroni-And-Cheese-mfs_006.jpg; https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F679c455ed74689800868fbbe020639f2%2F1653176636image.jpg; https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F679c455ed74689800868fbbe020639f2%2F165310488620220520_204159.jpg; https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F679c455ed74689800868fbbe020639f2%2F16450136607B0E9ECC-A28D-4F02-BAA9-F38C26122424.jpeg; https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F679c455ed74689800868fbbe020639f2%2F164409795116440979333786672039001412349626.jpg"
+    }
+
     return (
         <>
             <br></br>
@@ -9,14 +24,28 @@ const RecipeDetail = () => {
             <Link to="/groceries"><button>Back to Groceries (later an arrow to RecipesOverview)</button></Link>
             <div className="card">
                 <div className="card-image">
-                    <figure className="image is-4by3">
-                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                    <figure className="image">
+                        <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F03%2F19%2F238691-Simple-Macaroni-And-Cheese-mfs_006.jpg" alt="Placeholder image"/>
                     </figure>
+                </div>
+                <div className="column">
+                    <button className="button is-primary">Add to My Recipes</button>
+                    <button className="button is-primary">Mark as completed</button>
+                </div>
+                <header className="card-header">
+                    <h1 className="card-header-title title">{demoRecipe.name}</h1>  
+                </header>
+                <div className="columns">
+                        <div className="column">Preparation time: {demoRecipe.prep}</div>
+                        <div className="column">Cooking time: {demoRecipe.cook}</div>
+                        <div className="column">Total time: {demoRecipe.total}</div>
+                        <div className="column">Yield: {demoRecipe.yield}</div>
                 </div>
                 <div className="columns">
                     <div className="column is-two-fifths has-background-primary has-text-white has-text-left card-content">
                         <h2 className="subtitle has-text-white">Ingredients</h2>
                         <ul>
+                            <li>{demoRecipe.ingredients}</li>
                             <li>1 Eggplant</li>
                             <li>1 teaspoon vegetable oil</li>
                             <li>1/2 teaspoon cumin seeds</li>
@@ -24,7 +53,7 @@ const RecipeDetail = () => {
                     </div>
                     <div className="column has-text-left card-content">
                         <h2 className="subtitle has-text-primary">Directions</h2>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        <p>{demoRecipe.instructions}</p>
                     </div>
                 </div>
             </div>
