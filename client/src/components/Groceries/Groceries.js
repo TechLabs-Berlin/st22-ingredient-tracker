50f2054f28ff60f7cd821138bf65db6746711e6c
alj-b <aljoscha_b@icloud.com>
2022-07-20 16:33:43 +0200

    Search disabled until logged in (since it can't save anyway, I prefer to disable it) - Aljoscha

--- client/src/components/Groceries/Groceries.js
+++ client/src/components/Groceries/Groceries.js
@@ -140,7 +140,8 @@ const Groceries = () => {
                 <label className="label has-text-primary">Search</label>
                 <div className="field">
                     <p className="control">
-                        <input
+                        { groceries.length <= 0 &&
+                        <input disabled
                             value={term}
                             onChange={event => {
                                 setTerm(event.target.value);
@@ -153,8 +154,26 @@ const Groceries = () => {
                             }}
                             className="input is-rounded is-primary"
                             type="search"
-                            placeholder="search ingredient"
+                            placeholder="please log in first"
                         />
+                        }
+                        { groceries.length > 0 &&
+                        <input 
+                            value={term}
+                            onChange={event => {
+                                setTerm(event.target.value);
+                                if (event.target.value === "") {
+                                    listVisible = false;
+                                    setResults([]);
+                                } else {
+                                    listVisible = true;
+                                }
+                            }}
+                            className="input is-rounded is-primary"
+                            type="search"
+                            placeholder="search ingredients"
+                        />
+                        }
                     </p>
                 </div>
                 <div className={`${listVisible ? "box" : ""}`} id="resultBox">
