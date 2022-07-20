1440f3b29d2ac25e3218854e34ef4938360fcd69
saritaSchen <sarita.schenkermayr@gmail.com>
2022-07-20 10:14:04 +0200

    add new placeholder image

--- client/src/components/RecipeDetail/RecipeDetail.js
+++ client/src/components/RecipeDetail/RecipeDetail.js
@@ -2,22 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./RecipeDetail.css"
 import { getRecipeForId } from '../../API/recipes.api';
+import placeholder_fullHD from "../../images/placeholder_fullHD.png";
 
 const RecipeDetail = () => {
-    // const demoRecipe = {
-    //     "recipe_link": "https://www.allrecipes.com/recipe/238691/simple-macaroni-and-cheese/",
-    //     "name": "Simple Macaroni and Cheese Recipe",
-    //     "description": "Quick, easy, and tasty macaroni and cheese dish. Fancy, designer mac and cheese often costs forty or fifty dollars to prepare when you have so many exotic and expensive cheeses, but they aren't always the best tasting. This simple recipe is cheap and tasty.",
-    //     "prep": "10 mins",
-    //     "cook": "15 mins",
-    //     "additional": null,
-    //     "total": "25 mins",
-    //     "servings": "4",
-    //     "yield": "4 servings",
-    //     "ingredients": "1 (8 ounce) box elbow macaroni; cup butter;  cup all-purpose flour; teaspoon salt; ground black pepper to taste; 2 cups milk; 2 cups shredded Cheddar cheese",
-    //     "instructions": "Bring a large pot of lightly salted water to a boil. Cook elbow macaroni in the boiling water, stirring occasionally until cooked through but firm to the bite, 8 minutes. At the same time, melt butter in a saucepan over medium heat. Add flour, salt, and pepper and stir until smooth, about 5 minutes. Pour in milk slowly, while stirring continuously. Continue to cook and stir until mixture is smooth and bubbling, about 5 minutes, making sure the milk doesn't burn. Add Cheddar cheese and stir until melted, 2 to 4 minutes. Drain macaroni and fold into cheese sauce until coated.", "nutrition": "630 calories; protein 26.5g; carbohydrates 55g; fat 33.6g; cholesterol 99.6mg; sodium 777mg.",
-    //     "images_url": "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F03%2F19%2F238691-Simple-Macaroni-And-Cheese-mfs_006.jpg; https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F679c455ed74689800868fbbe020639f2%2F1653176636image.jpg; https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F679c455ed74689800868fbbe020639f2%2F165310488620220520_204159.jpg; https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F679c455ed74689800868fbbe020639f2%2F16450136607B0E9ECC-A28D-4F02-BAA9-F38C26122424.jpeg; https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F679c455ed74689800868fbbe020639f2%2F164409795116440979333786672039001412349626.jpg"
-    // }
 
     let id = '';
     // let selectedItems = [ 'thiswillalwaysjustreturnundefined' ];
@@ -72,9 +59,8 @@ const RecipeDetail = () => {
             <div className="card" id="recipeDetail">
                 <div className="card-image">
                     <figure className="image">
-                        <img
-                            src={`${!recipe.image_url ? "https://bulma.io/images/placeholders/1280x960.png" : recipe.image_url}`}
-                            alt="Placeholder image" />
+                        {recipe.image_url && <img src={recipe.image_url} alt="recipe image" />}
+                        {!recipe.image_url && <img src={placeholder_fullHD} alt="recipe image" />}
                     </figure>
                 </div>
                 <div id="recipeDetailButtons">
